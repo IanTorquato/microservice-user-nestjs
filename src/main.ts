49e9ea22b6cee3e58e61e79816961d5f330066a5
IanTorquato <ian@silbeck.com.br>
2021-03-29 11:23:49 -0300

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = 4010;
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 4010,
+    },
+  });
+
+  await app.listen(PORT, () =>
+    console.log(`Application-User running on port ${PORT}`),
+  );
+
+  Logger.log('User microservice running');
+}
+
+bootstrap();
